fde656a28db395aa9662ea3dc076b34a387e3086
ijagamino <ijagamino@gmail.com>
2025-12-14 23:47:59 +0800

    feat: add authentication redirection and backend middleware

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,13 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router";
-import DashboardPage from "../pages/dashboard/page";
-import DefaultLayout from "../layouts/default-layout";
-import RecordsPage from "../pages/records/page";
-import HomePage from "../pages/home/page";
-import UserIdPage from "@/pages/users/id/page";
-import LoginPage from "@/pages/login/page";
-import LoginLayout from "@/layouts/login-layout";
-import RegisterPage from "@/pages/register/page";
-import ProtectRoute from "@/components/protect-route";
+import { BrowserRouter, Route, Routes } from 'react-router'
+import DashboardPage from '@/pages/dashboard/page'
+import DefaultLayout from '@/layouts/default-layout'
+import RecordsPage from '@/pages/records/page'
+import HomePage from '@/pages/home/page'
+import UserIdPage from '@/pages/users/id/page'
+import LoginPage from '@/pages/login/page'
+import LoginLayout from '@/layouts/login-layout'
+import RegisterPage from '@/pages/register/page'
+import ProtectRoute from '@/components/protect-route'
+import LoginRouteWrapper from '@/components/login-route-wrapper.tsx'
 
 export default function AppRouter() {
   return (
@@ -21,44 +22,30 @@ export default function AppRouter() {
             </ProtectRoute>
           }
         >
-          <Route
-            index
-            element={<HomePage />}
-          />
-          <Route
-            path="dashboard"
-            element={<DashboardPage />}
-          />
+          <Route index element={<HomePage />} />
+          <Route path="dashboard" element={<DashboardPage />} />
 
           <Route path="records">
-            <Route
-              index
-              element={<RecordsPage />}
-            />
+            <Route index element={<RecordsPage />} />
           </Route>
 
           <Route path="users">
-            <Route
-              path=":id"
-              element={<UserIdPage />}
-            />
+            <Route path=":id" element={<UserIdPage />} />
           </Route>
         </Route>
 
         <Route
           path="/"
-          element={<LoginLayout />}
+          element={
+            <LoginRouteWrapper>
+              <LoginLayout />
+            </LoginRouteWrapper>
+          }
         >
-          <Route
-            path="login"
-            element={<LoginPage />}
-          />
-          <Route
-            path="register"
-            element={<RegisterPage />}
-          />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
