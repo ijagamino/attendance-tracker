6d838888b5ef0791fd5089acb4984b70b708108a
IJAGamino <ijagamino@gmail.com>
2025-12-15 17:16:52 +0800

    feat: improved time_hours computation and moved logic from backend to database level

--- server/db/seed.attendance-records.ts
+++ server/db/seed.attendance-records.ts
@@ -1,7 +1,5 @@
-import { formatToMonth } from '../lib/utils.ts'
 import connection from './db.ts'
 import type { ResultSetHeader } from 'shared/types/database'
-import { subDays } from 'date-fns'
 
 export default async function seedAttendanceRecords() {
   await connection.query<ResultSetHeader>(
@@ -22,51 +20,162 @@ export default async function seedAttendanceRecords() {
             ELSE 'On time'
           END)
           VIRTUAL,
-        total_hours TIME,
+        total_hours TIME GENERATED ALWAYS AS(
+            CASE
+            WHEN time_in IS NULL OR time_out IS NULL THEN NULL
+            ELSE
+              SEC_TO_TIME(
+                GREATEST(
+                  0,
+                  TIME_TO_SEC(TIMEDIFF(LEAST(time_out, '12:00:00'), time_in))
+                )
+                +
+                GREATEST(
+                  0,
+                  TIME_TO_SEC(TIMEDIFF(time_out, GREATEST(time_in, '13:00:00')))
+                )
+              )
+            END
+          ),
         FOREIGN KEY (user_id) references users(id),
         CONSTRAINT unique_user_date UNIQUE (user_id, date)
     )
     `
   )
 
-  const yesterday = subDays(new Date(), 1)
-
-  const today = new Date()
-
   await connection.execute<ResultSetHeader>(
-    `INSERT INTO attendance_records (user_id, date, time_in) VALUES (?, ?, ?)`,
-    [1, yesterday, '09:00:00']
+    `
+        INSERT INTO attendance_records (user_id, date, time_in)
+        VALUES
+          (?, ?, ?),
+          (?, ?, ?),
+          (?, ?, ?),
+          (?, ?, ?),
+          (?, ?, ?),
+          (?, ?, ?),
+          (?, ?, ?),
+          (?, ?, ?),
+          (?, ?, ?),
+          (?, ?, ?),
+          (?, ?, ?),
+          (?, ?, ?),
+          (?, ?, ?),
+          (?, ?, ?),
+          (?, ?, ?),
+          (?, ?, ?),
+          (?, ?, ?)
+        `,
+    [
+      2,
+      '2025-12-09',
+      '09:00:00',
+      2,
+      '2025-12-10',
+      '09:00:00',
+      2,
+      '2025-12-11',
+      '09:00:00',
+      2,
+      '2025-12-12',
+      '09:00:00',
+      2,
+      '2025-12-15',
+      '09:00:00',
+      3,
+      '2025-12-09',
+      '09:00:00',
+      3,
+      '2025-12-10',
+      '09:00:00',
+      3,
+      '2025-12-11',
+      '09:00:00',
+      3,
+      '2025-12-12',
+      '09:00:00',
+      3,
+      '2025-12-15',
+      '09:00:00',
+      4,
+      '2025-12-09',
+      '09:00:00',
+      4,
+      '2025-12-10',
+      '09:00:00',
+      4,
+      '2025-12-11',
+      '09:00:00',
+      4,
+      '2025-12-12',
+      '09:00:00',
+      4,
+      '2025-12-15',
+      '09:00:00',
+      5,
+      '2025-12-15',
+      '09:00:00',
+      6,
+      '2025-12-15',
+      '09:00:00',
+    ]
   )
 
   await connection.execute<ResultSetHeader>(
     `
     UPDATE attendance_records ar JOIN users u ON ar.user_id = u.id
     SET
-      time_out = ?,
-      total_hours = TIMEDIFF(time_out, time_in)
-    WHERE LOWER(u.username) = ? AND date = ?
+      time_out = ?
+    WHERE u.id = ? AND date = ?
     `,
-    ['18:00:00', 'ivan', formatToMonth(yesterday)]
-  )
-
-  await connection.execute<ResultSetHeader>(
-    `INSERT INTO attendance_records (user_id, date, time_in) VALUES (?, ?, ?)`,
-    [1, today, '09:00:00']
-  )
-
-  await connection.execute<ResultSetHeader>(
-    `INSERT INTO attendance_records (user_id, date, time_in) VALUES (?, ?, ?)`,
-    [2, today, '09:00:00']
+    ['18:00:00', 2, '2025-12-09']
   )
 
   await connection.execute<ResultSetHeader>(
     `
     UPDATE attendance_records ar JOIN users u ON ar.user_id = u.id
     SET
-      time_out = ?,
-      total_hours = TIMEDIFF(time_out, time_in)
-    WHERE LOWER(u.username) = ? AND DATE = ?
+      time_out = ?
+    WHERE u.id = ? AND DATE = ?
     `,
-    ['15:00:00', 'EDWARD', formatToMonth(today)]
+    ['15:00:00', 2, '2025-12-10']
+  )
+
+  await connection.execute<ResultSetHeader>(
+    `
+    UPDATE attendance_records ar JOIN users u ON ar.user_id = u.id
+    SET
+      time_out = ?
+    WHERE u.id = ? AND DATE = ?
+    `,
+    ['15:00:00', 2, '2025-12-11']
+  )
+  await connection.execute<ResultSetHeader>(
+    `
+    UPDATE attendance_records ar JOIN users u ON ar.user_id = u.id
+    SET
+      time_out = ?
+    WHERE u.id = ? AND DATE = ?
+    `,
+    ['15:00:00', 2, '2025-12-12']
+  )
+
+  await connection.execute<ResultSetHeader>(
+    `
+    UPDATE attendance_records ar JOIN users u ON ar.user_id = u.id
+    SET
+      time_out = ?
+    WHERE u.id = ? AND DATE = ?
+    `,
+    ['15:00:00', 2, '2025-12-15']
+  )
+
+  await connection.execute<ResultSetHeader>(
+    `
+    UPDATE attendance_records ar JOIN users u ON ar.user_id = u.id
+    SET
+      time_out = ?
+    WHERE u.id = ? AND DATE = ?
+    `,
+    ['15:00:00', 3, '2025-12-09']
   )
 }
